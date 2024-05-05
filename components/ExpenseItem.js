9727ae4731c1e2a23a5b6754cc248d92f2a00dad
Ujjawal Kaul <ujjwal171@outlook.com>
2024-05-05 16:33:55 +0530

    Added dynamic title

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -4,10 +4,12 @@ import { GlobalStyles } from '../styles';
 import getFormattedDate from '../util/date';
 import { useNavigation } from '@react-navigation/native';
 
-export default function ExpenseItem({ description, amount, date }) {
+export default function ExpenseItem({ id, description, amount, date }) {
   const navigation = useNavigation();
   function expensePressHandler() {
-    navigation.navigate('ManageExpense');
+    navigation.navigate('ManageExpense', {
+      expenseId: id,
+    });
   }
   return (
     <Pressable
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 
-export default function ManageExpense() {
+export default function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>ManageExpense</Text>
